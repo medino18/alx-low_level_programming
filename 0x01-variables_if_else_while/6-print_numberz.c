18f348396ce4aad0f3fdee3a957a3759b9990684
Ahmed <meddymed1990@yahoo.com>
2022-09-12 13:21:26 -0700

    number 2 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print digits using putchar
+ * description - same as main
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
