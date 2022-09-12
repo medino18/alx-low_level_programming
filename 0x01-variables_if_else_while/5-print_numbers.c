998409b1f40885db5b1ecb36d69c69f641571060
Ahmed <meddymed1990@yahoo.com>
2022-09-12 13:12:11 -0700

    number task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- print zero to nine
+ * description- same as main
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		printf("%i", num);
+	}
+	printf("\n");
+	return (0);
+}
