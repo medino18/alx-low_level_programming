8dee67f7222b40560bad83c7084c9fca8d4e2f8d
Ahmed <meddymed1990@yahoo.com>
2022-09-13 16:59:05 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @n: variable
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
