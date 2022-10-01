e7279ebb744bc458a9f647813de320ec90bcff00
Ahmed <meddymed1990@yahoo.com>
2022-10-01 11:06:26 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcat - fxn that concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: end string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
