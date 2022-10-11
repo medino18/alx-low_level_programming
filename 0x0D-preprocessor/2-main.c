dac5394d67e13f9312cf7c7ef1f70a024ec6d99f
Ahmed <meddymed1990@yahoo.com>
2022-10-10 17:37:59 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file program was
+ * compiled from, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
